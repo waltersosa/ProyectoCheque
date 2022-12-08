6f6100e00d1758addd57c2824c9caec4f1cb6f26
waltersosa <santiagososamejia@gmail.com>
2022-12-08 12:59:18 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoCheque/Funciones/Utilidades.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones
+{
+    public static class Utilidades
+    {
+        private static string[] unidades = {"Cero", "Uno", "Dos", "Tres",
+        "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez", "Once",
+        "Doce", "Trece", "Catorce", "Quince", "Diesiseis", "Diesiciete",
+        "Diesiocho", "Diesinueve"};
+
+        private static string[] decenas = {"", "Diez", "Veinte", "Treinta", 
+        "Cuarenta", "Cincuenta", "Sesenta", "Setenta", "Ochenta", "Noventa" };
+
+        private static string[] centenas = {"", "Cien", "Dosciento", "Treciento", 
+        "Cuatrociento", "Quiniento", "Seisciento", "Sieteciento", "Ochociento", 
+        "Noveciento" };
+        public static string getUnidades (int num)
+        {
+            string aux = " ";
+            aux = unidades[num];
+            return aux;
+        }
+
+        public static string getDecenas (int num)
+        {
+            string aux = " ";
+            int x = num / 10;
+            int res = num % 10;
+
+            //Si es de 0 a 19 salen las unidades que es el primer array
+            if (num < 20)
+            
+                aux = getUnidades(num);
+  
+            //Y si no salen las centenas que estan en el segundo array
+            else
+            {
+                string uni = res == 0 ? "" : " y " + getUnidades(res);
+                aux = decenas[x] + uni;
+            }
+       
+            return aux;
+        }
+        public static string getCentenas(int num)
+        {
+            string aux = " ";
+            int x = num / 100;
+            int res = num % 100;
+
+            //Si es de 0 a 19 salen las unidades que es el primer array
+            if (num < 100)
+
+                aux = getUnidades(num);
+
+            //Y si no salen las centenas que estan en el segundo array
+            else
+            {
+                string dec = res == 0 ? "" : " y " + getUnidades(res) + " y " + getCentenas (res);
+                aux = decenas[x] + dec;
+            }
+
+            return aux;
+        }
+
+    }
+}
---
+++ ProyectoCheque/frmCheque.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCheque
+{
+    public partial class frmCheque : Form
+    {
+        public frmCheque()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCheque_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            if (this.txtCantidad.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingrese una cantidad");
+                this.txtCantidad.Focus();
+                return;
+
+            }
+            //Actividad pa los estudiantes:
+            //Validar que txt cantidad tenga valores  numericos con truParse
+            //Validar que se haya ingresado el nombre
+            int num = Int32.Parse(txtCantidad.Text);
+            string res = Funciones.Utilidades.getDecenas(num);
+            this.txtResultado.Text = res;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
