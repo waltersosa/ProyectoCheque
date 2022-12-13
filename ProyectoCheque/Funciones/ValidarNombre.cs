5d3ce40b0eca93fb9a8756be6cacb62f7b78d9dd
Santiago Sosa Mejía <105513565+waltersosa@users.noreply.github.com>
2022-12-13 18:37:13 -0500

    Add files via upload

---
+++ ProyectoCheque/Funciones/ValidarNombre.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCheque
+{
+    public static class ValidarUsuario
+    {
+        public static bool isString(string usuario)
+        {
+            foreach (char c in usuario)
+            {
+                if (!(char.IsLetter(c)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
